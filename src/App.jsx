0cb55d37f3b67d43f70704d3d1326eb42855b214
Muhammad SA <MuhammadSA211999>
2023-04-13 03:27:36 +0600

    fix firebase error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import Home from "./components/Home"
-import app from "./firebase/firbase.config"
-
 function App() {
-
   return (
     <div>
-      <h1>LWS QUIZZ APP</h1>
-      <Home></Home>
+      {/* <h1>LWS QUIZZ APP</h1>
+      <Home></Home> */}
     </div>
   )
 }
